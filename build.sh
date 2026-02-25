524fbf6502ac8443393efa81c1565c85aa0681bd
Neolux Lee <neolux_lee@outlook.com>
2026-02-26 00:01:32 +0800

    chore: 自动构建脚本和DLL名单

--- build.sh
+++ build.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-rm -fr build package
 
-cmake -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE=cmake/mingw64.cmake -DFEMAPP_ADD_WIN32=ON -DCMAKE_BUILD_TYPE=Release
+if [ -z "$BUILD_DIR" ]; then
+    BUILD_DIR=build
+fi
+if [ -z "$PACK_DIR" ]; then
+    PACK_DIR=package
+fi
 
-cmake --build build -j
+rm -fr $BUILD_DIR $PACK_DIR
+
+cmake -B $BUILD_DIR -G Ninja -DCMAKE_TOOLCHAIN_FILE=cmake/mingw64.cmake -DFEMAPP_ADD_WIN32=ON -DCMAKE_BUILD_TYPE=Release
+
+cmake --build $BUILD_DIR -j
 
 
-mkdir -pv package
-mkdir -pv package/translations
+mkdir -pv $PACK_DIR
+mkdir -pv $PACK_DIR/translations
 
-cp -r build/FemApp.exe package/
-cp -r /usr/x86_64-w64-mingw32/lib/qt6/plugins/* package
-cp -r translations/*.qm package/translations
+cp -r $BUILD_DIR/FemApp.exe $PACK_DIR/
+cp -r /usr/x86_64-w64-mingw32/lib/qt6/plugins/* $PACK_DIR
+cp -r translations/*.qm $PACK_DIR/translations
 
-env DLLPATH=/usr/x86_64-w64-mingw32/bin:/usr/x86_64-w64-mingw32/lib:./build \
-x86_64-w64-mingw32-dllcopy package/FemApp.exe ./dll.txt --verbose -r
+env DLLPATH=/usr/x86_64-w64-mingw32/bin:/usr/x86_64-w64-mingw32/lib:$BUILD_DIR \
+x86_64-w64-mingw32-dllcopy $PACK_DIR/FemApp.exe ./dll.txt --verbose -r
 
-zip -j femapp_package_v$(date +%Y%m%d).zip package/*
+zip -j femapp_package_v$(date +%Y%m%d).zip $PACK_DIR/*
