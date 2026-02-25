8adca7f053c8af9d226fd631016e38a45c2cda19
Neolux Lee <neolux_lee@outlook.com>
2026-02-25 12:53:07 +0800

    chore: add single-click build and pack script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -fr build package
+
+cmake -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE=cmake/mingw64.cmake -DFEMAPP_ADD_WIN32=ON -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build -j
+
+
+mkdir -pv package
+
+cp -r build/FemApp.exe package/
+cp -r /usr/x86_64-w64-mingw32/lib/qt6/plugins/* package
+
+export DLLPATH=/usr/x86_64-w64-mingw32/bin:/usr/x86_64-w64-mingw32/lib:./build
+
+x86_64-w64-mingw32-dllcopy package/FemApp.exe ./dll.txt --verbose -r
+
+zip -j femapp_package_v$(date +%Y%m%d).zip package/*
--- packer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -frv packaging
-mkdir -pv packaging
-cp -v build/FemApp.exe packaging/
-
-x86_64-w64-mingw32-wine windeployqt6 ./packaging/FemApp.exe
